3e918396bf60a11b93ed93554ea6c9f2e19dc643
Nour-Alhajali <nour.dev.work@gmail.com>
2025-03-22 19:26:08 +0300

    feat: Add initial grid functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
+const gridElement = document.querySelector(".paint-paper__grid");
 
+constructPixelGrid(16);
+
+function getNewPixel(gridWidthInPixels, gridHeightInPixels) {
+  //Get pixel width and height according to actual canvas size the the size in pixel elements
+  const gridWidth = gridElement.getBoundingClientRect().width;
+  const gridHeight = gridElement.getBoundingClientRect().height;
+
+  let pixelWidth = gridWidth / gridWidthInPixels;
+  const pixelHeight = gridHeight / gridHeightInPixels;
+
+  const pixelElement = document.createElement("div");
+  pixelElement.style.cssText = `border: 1px solid #1f2024; width: ${pixelWidth}px; height: ${pixelHeight}px; flex: 0 0 auto`;
+  return pixelElement;
+}
+
+function constructPixelGrid(width, height = width) {
+  // Spawn pixel elements the size of the canvas
+  for (let i = 0; i < width * height; i++) {
+    gridElement.appendChild(getNewPixel(width, height));
+  }
+}
